3a2ec58354b8947ee74a3558745ab0da66e78a62
Helmer Salas <yonnich@gmail.com>
2022-07-03 18:17:22 -0700

    Actualizacion
    modificacion del archivo ignore

--- Cola.cs
+++ Cola.cs
@@ -7,18 +7,16 @@ using System.Collections;
 
 namespace EjemploCola {
 
-
 public class Cola {
 
 static void Main(string[] args){
 
 string palabra;
 
-Queue cola = new Queue();
+  Queue cola = new Queue();
   cola.Enqueue("Hola ");
   cola.Enqueue("buenos ");
   cola.Enqueue("dias.");
-
 
   for (byte i =0; i < 3;i++) {
     palabra = (string) cola.Dequeue();
