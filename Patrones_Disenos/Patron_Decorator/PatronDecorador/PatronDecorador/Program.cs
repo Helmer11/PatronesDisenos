abc1ac16309b243990a5541efd2e4cb6e86845d4
Helmer Salas <yonnich@gmail.com>
2023-10-18 01:38:52 -0700

    mosificacion de carpeta

--- Patrones_Disenos/Patron_Decorator/PatronDecorador/PatronDecorador/Program.cs
+++ Patrones_Disenos/Patron_Decorator/PatronDecorador/PatronDecorador/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+using PatronDecoratorC;
+
+BebidaComponent cafe = new CafeDescafeinado();
+cafe = new Crema(cafe);
+cafe = new Edulcorante(cafe);
+cafe = new Canela(cafe);
+
+Console.WriteLine($"Producto:  {cafe.Descripcion} tiene un costo de : ${cafe.Costo}");
+
+cafe = new CafeExpresso();
+cafe = new Leche(cafe);
+cafe = new Edulcorante(cafe);
+cafe = new Canela(cafe);
+
+Console.WriteLine($"Producto:  {cafe.Descripcion} tiene un costo de : ${cafe.Costo}");
+
+cafe = new CafeSolo();
+cafe = new Edulcorante(cafe);
+
+Console.WriteLine($"Producto:  {cafe.Descripcion} tiene un costo de : ${cafe.Costo}");
+
+
+Console.ReadKey();
+
