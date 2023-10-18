dd922f578ffb833769d516556566fd8fdd5b7552
Helmer Salas <yonnich@gmail.com>
2023-10-17 17:46:43 -0700

    Patron de diseno decorador

--- Patrones_Disenos/Patron_Factory_method/PatronFactory_Mathod/PatronFactory_Mathod/PizzeriaArgentina.cs
+++ Patrones_Disenos/Patron_Factory_method/PatronFactory_Mathod/PatronFactory_Mathod/PizzeriaArgentina.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace PatronFactory_Mathod
 {
-    internal class PizzeriaArgentina
+    internal class PizzeriaArgentina : Pizzeria
     {
+        public override Pizza CrearPizza(string tipo)
+        {
+            if (tipo == "cancha")
+            {
+                return new PizzaCancha("Italia");
+            }
+            else if (tipo == "napo")
+            {
+                return new PizzaNapolitana("Italia");
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
